62984fe18aac9db4a04f9f009e111fc09ed050a6
jgan <jgan@roku.com>
2026-06-13 11:04:29 -0700

    Redirect / to /web

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "prisma"],
   },
+  async redirects() {
+    return [
+      { source: "/", destination: "/web", permanent: false },
+    ];
+  },
 };
 
 module.exports = nextConfig;
